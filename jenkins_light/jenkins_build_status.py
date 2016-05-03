391264c58581ffc4184a41e279e590008ad6d42b
rbanikaz <rezapb36@gmail.com>
2016-05-02 22:49:01 -0700

    a hacky but working version

--- jenkins_light/jenkins_build_status.py
+++ jenkins_light/jenkins_build_status.py
@@ -1,10 +1,16 @@
 import base64
 import json 
 import sys
+import os
 import urllib2
 from PIL import ImageFont
 from PIL import Image
 from PIL import ImageDraw
+
+import time
+from rgbmatrix import Adafruit_RGBmatrix
+
+
 
 def get_build_status():
   with open('auth.json', 'r') as f:
@@ -16,19 +22,21 @@ def get_build_status():
   jobName = config["job"]
 
   try:
-    url = jenkinsUrl + jobName + "/lastBuild/api/json"
+    url = jenkinsUrl + "job/" + jobName + "/lastBuild/api/json"
+    print header+" "+url
     req = urllib2.Request(url)
     req.add_header('Authorization', header)
-    resp = urllib2.urlopen(req)
+    resp = urllib2.urlopen(req).read()
   except urllib2.HTTPError, e:
     print "URL Error: " + str(e.code) 
     print "      (job name [" + jobName + "] probably wrong)"
     return "ERROR"
 
   try:
-    buildStatus = json.load( resp )
-  except:
+    buildStatus = json.loads( resp )
+  except ValueError as err:
     print "Failed to parse json"
+    print err
     return "ERROR"
 
   if buildStatus["building"]:
@@ -41,13 +49,13 @@ def get_build_status():
     return "ERROR"
 
 
-QM2="QM2 "
+QM2="QM2"
 CHECKMARK= u'\u2714'
 CROSS= u'\u2716'
 CIRCLE = u'\u2b55'
 
 repo = QM2
-build_status = "FAILUR"#get_build_status()
+build_status = "FAILURE"#get_build_status()
 freesans = ImageFont.truetype("FreeSans.ttf")
 symbola = ImageFont.truetype("Symbola.ttf", 16)
 
@@ -68,15 +76,38 @@ if symbol is not None:
   width += symbola.getsize(symbol)[0]
 
 
-im = Image.new("RGB", (width, 16), "black")
-draw = ImageDraw.Draw(im)
+matrix = Adafruit_RGBmatrix(16, 1)
+
+
+image = Image.new("RGB", (width, 16), "black")
+draw = ImageDraw.Draw(image)
 
 draw.text((0, 0), repo, color, font=freesans)
 
 if symbol is not None:
   draw.text((width_repo, 0), symbol, color, font=symbola)
 
-im.save("blah.png", "PNG")
+for n in range(32,0 , -1):
+  matrix.Clear()
+  matrix.SetImage(image.im.id, n, 0)
+  time.sleep(0.05)
 
+time.sleep(1)
+matrix.Clear()
+time.sleep(1)
+matrix.SetImage(image.im.id, 0, 0)
+time.sleep(1)
+matrix.Clear()
+time.sleep(1)
+matrix.SetImage(image.im.id, 0, 0)
+time.sleep(1)
+matrix.Clear()
+time.sleep(1)
+matrix.SetImage(image.im.id, 0, 0)
+time.sleep(1)
+matrix.Clear()
+time.sleep(1)
+matrix.SetImage(image.im.id, 0, 0)
+time.sleep(100)
 
 
