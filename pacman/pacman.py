f6b094147405162d88789a1b7b82bac222b5fcde
Reza Banikazemi <reza@quanticmind.com>
2016-05-04 20:14:53 -0700

    little bit cleaner code

--- pacman/pacman.py
+++ pacman/pacman.py
@@ -9,17 +9,18 @@ from PIL import Image
 from PIL import ImageDraw
 from rgbmatrix import Adafruit_RGBmatrix
 
-radius = 16
+pac_size = 16
 
 def get_pacman(mouth):
-  image = Image.new("RGB", (radius , radius), "black")
+  image = Image.new("RGB", (pac_size , pac_size), "black")
   draw = ImageDraw.Draw(image)
 
-  draw.ellipse((1, 1, radius , radius), fill = 'yellow', outline ='black')
+  draw.ellipse((1, 1, pac_size , pac_size), fill = 'yellow', outline ='black')
+  draw.ellipse((8,4,8,5), fill = 'black', outline = 'black')
   if mouth == "OPEN":
-    draw.pieslice((1, 1, radius, radius ), 160, 220, fill="black", outline ='black')
+    draw.pieslice((1, 1, pac_size, pac_size ), 160, 220, fill="black", outline ='black')
   else:
-    draw.pieslice((1, 1, radius, radius ), 175, 185, fill="black", outline ='black')
+    draw.pieslice((1, 1, pac_size, pac_size ), 178, 182, fill="black", outline ='black')
   return image
 
 pacman_open = get_pacman("OPEN")
@@ -27,12 +28,25 @@ pacman_closed = get_pacman("CLOSED")
 
 matrix = Adafruit_RGBmatrix(16, 1)
 
-span = (32,31,30,29, 24,23,22,21, 16,15,14,13, 8,7,6,5, 1,-1,-2,-3, -8,-9,-10,-11)
+pellet = Image.new("RGB", (4, 4), "black")
+draw = ImageDraw.Draw(pellet)
+draw.ellipse((1,1,4,4), fill= 'blue', outline='blue')
 
-for n in range(32, -radius, -1):
+section_width = 5
+
+for n in range(32, -pac_size, -1):
   matrix.Clear()
-  if n in span:
+  if n > 6:
+    matrix.SetImage(pellet.im.id, 6, 5)
+  if n > 12:
+    matrix.SetImage(pellet.im.id, 12, 5)
+  if n > 18:
+    matrix.SetImage(pellet.im.id, 18, 5)
+  if n > 24:
+    matrix.SetImage(pellet.im.id, 24, 5)
+  section = n / section_width
+  if section % 2 == 0:
     matrix.SetImage(pacman_open.im.id, n, 0)
   else:
     matrix.SetImage(pacman_closed.im.id, n, 0)
-  time.sleep(0.05)
+  time.sleep(0.08)
